a78fb8fe3292478eea929a558d71da357728166f
ioRaid <30330862+ioRaid@users.noreply.github.com>
2017-09-27 01:50:50 -0500

    Update alamofire.rb

--- alamofire.rb
+++ alamofire.rb
@@ -1,5 +1,5 @@
 p "make up cartfile"
-File.write("Cartfile", 'github "Alamofire/Alamofire" ~> 4.5') 
+File.write("Cartfile", 'github "Alamofire/Alamofire"') 
 p "carthage update start"
 `carthage update`
 p "carthage update finish"
