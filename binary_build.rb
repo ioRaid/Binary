2a4b5be914f5f8423b04e0d9d86658b1f83bcdd1
ioRaid <30330862+ioRaid@users.noreply.github.com>
2017-09-27 01:21:46 -0500

    Update binary_build.rb

--- binary_build.rb
+++ binary_build.rb
@@ -1,7 +1,6 @@
 p "make up cartfile"
 File.write("Cartfile", 'github "SnapKit/SnapKit" ~> 4.0.0') 
 File.write("Cartfile", 'github "Alamofire/Alamofire" ~> 4.5') 
-File.write("Cartfile", 'github "Mantle/Mantle"') 
 # 生成一个叫 Cartfile 的文件，让里面写入后面的字符串
 p "carthage update start"
 `carthage update`
