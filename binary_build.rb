520a757a9ff910c962a5d2e35c3d5b41188fdc61
iOS-xxx <nmtx@iOS-xxxdeiMac.local>
2017-09-27 13:58:53 +0800

    add

--- binary_build.rb
+++ binary_build.rb
@@ -1,5 +1,6 @@
 p "make up cartfile"
 File.write("Cartfile", 'github "Mantle/Mantle"') 
+File.write("Cartfile", 'github "ReactiveX/RxSwift"') 
 # 生成一个叫 Cartfile 的文件，让里面写入后面的字符串
 p "carthage update start"
 `carthage update`
